89436c6f13df5a9b666b161c5b49c8b411dfa917
gabzon <gab.zambrano@gmail.com>
2016-04-23 13:59:08 +0200

    sage updated

--- templates/footer.php
+++ templates/footer.php
@@ -1,68 +1,45 @@
-<footer class="ui basic attached borderless" class="content-info" role="contentinfo">
-	<div class="ui centered grid bg-3">
-		<div class="twelve wide column">
-			<br>
-			<h2 class="ui aligned medium center inverted header">
-				<div class="content">
-					<i>GARDONS CONTACT</i><br><br>
-					<b>C'est ici que cela se passe</b>
-					<br>
-					<div class="sub header">
-         				<i class="massive angle down icon bright"></i>
-					</div>
-				</div>
-			</h2>
-		</div>
-	</div>
-	<div class="ui stackable four column grid bg-1 margin-big">
-		<div class="column">
-			<h2 class="ui center medium aligned icon inverted header">
-				<i class="circular home icon main-font-color inverted"></i>
-				<div class="content">
-					Adresse de l'école
-					<div class="sub header"><br>
-						<a target="_blank" href="https://www.google.ch/maps/place/Caf%C3%A9+Julienne/@46.1697515,6.1198219,15z/data=!4m2!3m1!1s0x0:0x5072048b725adc3d">Café Julienne <i class="marker icon small aligned"></i></a><br>
-						Route de Saint-Julien 116<br>
-						1228 Plan-les-Ouates<br>
-						Genève, Suisse
-					</div>
-				</div>
-			</h2>
-		</div>
-		<div class="column">
-			<h2 class="ui center medium aligned icon inverted header">
-				<i class="circular call icon main-font-color "></i>
-				<div class="content">
-					Téléphone
-					<div class="sub header"><br><a href="tel:+41799592269">+41 79 959 22 69</a></div>
-				</div>
-			</h2>
-		</div>
-		<div class="column">
-			<h2 class="ui center medium aligned icon inverted header">
-				<i class="circular envelope icon main-font-color"></i>
-				<div class="content">
-					Email
-					<div class="sub header"><br><script type="text/javascript">// <![CDATA[
+<footer class="ui segment inverted basic basic vertical">
+	<br>
+	<br>
+	<div class="ui container">
+		<div class="ui four column grid stackable">
+			<div class="column">
+				<a class="item" href="<?php echo site_url(); ?>" >
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo-isma-testw.png"  class="ui small middle aligned image"/>
+				</a>
+			</div>
+			<div class="column">
+				<table>
+					<tr>
+						<td valign="top"><i class="marker icon small aligned"></i></td>
+						<td>
+							<a target="_blank" href="https://www.google.ch/maps/place/Caf%C3%A9+Julienne/@46.1697515,6.1198219,15z/data=!4m2!3m1!1s0x0:0x5072048b725adc3d">Café Julienne</a><br>
+							Route de Saint-Julien 116,<br>
+							1228 Plan-les-Ouates, CH
+						</td>
+					</tr>
+				</table>
+
+
+			</div>
+			<div class="column">
+				<i class="call icon"></i>
+				<a href="tel:+41799592269">+41 79 959 22 69</a><br>
+				<i class="envelope icon"></i>
+				<script type="text/javascript">// <![CDATA[
 a = 'ismaelk'+'.co'+'m'
 b = 'c'+'ontact';
 o = 'mai'+'lt'+'o:';
 z=(b+'@'+a);
 document.write('<'+'a hr'+'ef="' + o + z + '">'+z+'</'+'a>');
-// ]]&gt;</script></div>
-				</div>
-			</h2>
-		</div>
-		<div class="column">
-			<h2 class="ui center medium aligned icon inverted header">
-				<i class="circular facebook icon main-font-color"></i>
-				<div class="content">
-					Facebook
-					<div class="sub header"><br>
-					<a href="https://www.facebook.com/ismaelk.saxo">facebook.com/ismaelk.saxo</a></div>
-				</div>
-			</h2>
-		</div>
+// ]]&gt;</script>
+			</div>
+			<div class="column">
+				<i class="facebook icon"></i>
+				<a href="https://www.facebook.com/ismaelk.saxo">facebook.com/ismaelk.saxo</a>
+			</div>
+		</div>		
 	</div>
+	<br>
+	<br>
 </footer>
-</div>
